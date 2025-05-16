67c45e8f157860ca92c71254776dd06a21ec517e
Mate <mateasanidze808@gmail.com>
2025-05-16 22:39:19 +0400

    Registration Form

--- Auth/settings.py
+++ Auth/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.staticfiles',
     'Users',
 ]
@@ -119,9 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def register(request):
+    form = RegisterForm()
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "❌ Email already in use.")
+            return render(request, 'main/register.html', {'form': form})
+
+        if form.is_valid():
+            form.save()
+        else:
+            messages.error(request, "❌ There is something wrong with password")
+    else:
+        form = RegisterForm()
+
+    return render(request, 'main/register.html', {'form': form})
